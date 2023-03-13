68cbe476f06ba239d6b7e2cc5e33e45d525899b1
Slepy <92794194+Slepy1@users.noreply.github.com>
2023-03-13 14:34:08 +0000

    a lot of changes
    todo - recording of data (basic csv is implemented) and testing

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,38 +6,84 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.net.wifi.WifiNetworkSuggestion
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.*
+import java.nio.file.Paths
+import java.time.Duration
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+var stopClicked = false
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //check permissions xdxd
+        //check and/or ask for permissions
         checkPermissions()
 
+        //create some global values and managers
+        //val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        //wait at least 10 seconds
+        //remember to disable throttling in Developer Options > Networking > Wi-Fi scan throttling (normally only allows 4 scans per 2 minutes)
+        var TimeNow = System.currentTimeMillis()
+        var TimeFuture = TimeNow + 1
+
+//put it into a corutine cause cannot delay main function
+
+
+
+        //scanWifi()
+
+
+
+        findViewById<Button>(R.id.buttonStop).setOnClickListener {
+
+            stopClicked = true
+
+        }
+
+
+
         //get password and name from editext
-        var WiFi_name = findViewById(R.id.WiFi_network) as EditText
-        var WiFi_password = findViewById(R.id.WiFi_passowrd) as EditText
+        //var WiFi_name = findViewById(R.id.WiFi_network) as EditText
+        //var WiFi_password = findViewById(R.id.WiFi_passowrd) as EditText
 
-
+        Log.d("test1", "test1")
         //val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
         //when connect button is pressed
         findViewById<Button>(R.id.ConnectButton).setOnClickListener {
+            Log.d("test1", "test")
+            //isDeviceOnline(this)
+            stopClicked = false
+
+            GlobalScope.launch  {main_coroutine()}
 
             //save name and password in this class
-            val suggestWifi = WifiNetworkSuggestion.Builder()
-                .setSsid(WiFi_name.text.toString())
-                .setWpa2Passphrase(WiFi_password.text.toString())
-                .build()
+          //  val suggestWifi = WifiNetworkSuggestion.Builder()
+           //     .setSsid(WiFi_name.text.toString())
+           //     .setWpa2Passphrase(WiFi_password.text.toString())
+           //     .build()
 
             //create wifi manager
             //val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
@@ -51,32 +97,32 @@ class MainActivity : AppCompatActivity() {
 
 
 
-            val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+          //  val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
-            val wifiScanReceiver = object : BroadcastReceiver() {
-                override fun onReceive(context: Context, intent: Intent) {
-                    val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
-                    Log.d("test", success.toString())
-                    if (success) {
-                        scanSuccess()
-                        val results = wifiManager.scanResults
-                        Log.d("test", results.toString())
-                    } else {
-                        scanFailure()
-                    }
-                }
-            }
+          //  val wifiScanReceiver = object : BroadcastReceiver() {
+           //     override fun onReceive(context: Context, intent: Intent) {
+           //         val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+           //         Log.d("test", success.toString())
+           //         if (success) {
+           //             scanSuccess()
+           //             val results = wifiManager.scanResults
+            //            Log.d("test", results.toString())
+           //         } else {
+           //             scanFailure()
+           //         }
+           //     }
+          //  }
 
-            Log.d("test", "3")
-            val intentFilter = IntentFilter()
-            intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-            this.registerReceiver(wifiScanReceiver, intentFilter)
+           // Log.d("test", "3")
+          //  val intentFilter = IntentFilter()
+           // intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+          //  this.registerReceiver(wifiScanReceiver, intentFilter)
 
-            val success = wifiManager.startScan()
-            if (!success) {
+          //  val success = wifiManager.startScan()
+          //  if (!success) {
                 // scan failure handling
-                scanFailure()
-            }
+          //      scanFailure()
+         //   }
 
             //WifiManager.startScan()
 
@@ -100,23 +146,242 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-    private fun scanSuccess() {
-      //  val results = wifiManager.scanResults
-        Log.d("test", "1")
-
+    private fun isDeviceOnline(context: Context): Boolean {
+        val connManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connManager.getNetworkCapabilities(connManager.activeNetwork)
+            if (networkCapabilities == null) {
+                Log.d("test1", "Device Offline")
+                return false
+            } else {
+                Log.d("test1", "Device Online")
+                if (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
+                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED)
+                ) {
+                    Log.d("test1", "Connected to Internet")
+                    return true
+                } else {
+                    Log.d("test1", "Not connected to Internet")
+                    return false
+                }
+            }
+        }
+     else {
+        Log.d("test1", "Device Offline")
+        return false
     }
 
-    private fun scanFailure() {
-        // handle failure: new scan did NOT succeed
-        // consider using old scan results: these are the OLD results!
-       // val results = wifiManager.scanResults
-        Log.d("test", "2")
+}
 
+
+
+
+    private fun scanWifi(): MutableList<ScanResult>? { //scan for all detectable access points
+        //creates and intent to scan wifi and a receiver to get the scan results
+        //works only on new versions of android, on android oreo it just returns empty list
+        val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        Log.d("test1", "Searching for hotspots")
+        val wifiScanReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        this.registerReceiver(wifiScanReceiver, intentFilter)
+        val success = wifiManager.scanResults
+        Log.d("test1", success.toString())
+        //if empty = fail or your in a desert
+        return success
     }
 
 
+    suspend fun main_coroutine() {//run this function as a coroutine so that delay can be used
 
+        val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        var TimeNow = System.currentTimeMillis()
+        var TimeFuture = TimeNow + 10000
+        var ssids: Queue<String> = LinkedList<String>()
+        var scanResults: MutableList<ScanResult>?
+        var suggestionsList = ArrayList<WifiNetworkSuggestion>()
+        var recentlyTried: Queue<String> = LinkedList<String>()
+
+        while (!stopClicked){
+
+            TimeNow = System.currentTimeMillis()
+            TimeFuture = TimeNow + 10000
+            ssids.clear()
+            suggestionsList.clear()
+
+            //TimeNow = System.currentTimeMillis()//update current time //remember to update future timer later
+
+            //search for a suitable hotspot
+            scanResults = scanWifi()
+            //var scanResults = scanWifi()
+
+           // Log.d("test1", "test = ${scanResults?.get(0)}")
+           // val ssids = scanResults?.map { it.SSID }
+            //Log.d("test1", ssids?.get(0).toString())
+            //val ssids: Queue<String> = LinkedList<String>()
+
+            if (scanResults != null) {//need this check because android studio cries about it
+                for (result in scanResults) {
+                    val capabilities = result.capabilities
+                    val ssid = result.SSID
+
+                    //val ssids: Queue<String> = LinkedList<String>()
+
+                    val isPasswordProtected = capabilities.contains("WPA") || capabilities.contains("WEP") //if capabilities contain word wpa or wpe then the wifi is password protected
+                    //Log.d("test1", ssid.toString() + " " + isPasswordProtected.toString())
+                    if (capabilities.contains("WPA") || capabilities.contains("WEP")) {
+
+
+                        Log.d("test1", ssid.toString() + " is password protected")
+                    }
+                    else {
+                        Log.d("test1", ssid.toString() + " is not password protected")
+                        ssids.add(ssid)
+                    }
+                    }
+
+
+                if (ssids != null) {
+                    Log.d("test1", "found " + ssids.size + " paswordless hotspots")
+
+
+                   // val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+                    //var suggestionsList = ArrayList<WifiNetworkSuggestion>()
+
+                    run breaking@ {
+                    ssids?.forEach {//a fancy loop for queues
+                            x ->
+                        recentlyTried.forEach { y ->
+                            if (x == y) {
+                                //do nothing
+                            }
+                            recentlyTried.add(x)
+                            //add to the list
+                            if (recentlyTried.size > 3){
+                                recentlyTried.remove()
+                            }
+                            //if the list is more than 3 pop 1
+                            //try
+                            wifiManager.addNetworkSuggestions(suggestionsList)
+                            val suggestWifi = WifiNetworkSuggestion.Builder()
+                                .setSsid(x)
+                                .build()
+                            suggestionsList.add(suggestWifi)
+                            return@breaking
+                        }
+                    }
+
+
+                       // val suggestWifi = WifiNetworkSuggestion.Builder()
+                          //  .setSsid(x)
+                         //   .build()
+                      //  suggestionsList.add(suggestWifi)
+                    }
+
+                    ////////////////////////////
+                    //wifiManager.addNetworkSuggestions(suggestionsList);
+
+                }
+                //Log.d("test1", "All hotspots are password protected")
+
+
+
+                }
+                //Log.d("test1", "no hotspots detected")
+
+
+            Log.d("test1", "123")
+            if (suggestionsList.size > 0){
+                Log.d("test1", "1234")
+
+
+                //problem ahead
+                //basically sometimes android insists on using a certain access point
+                //for example at lidl near dundee uni, i know i can log in into lidls wifi but android
+                //insists on using some wierd wifi that i cannot log into
+                //i can either pick a random access point or have another list to follow ssid of access points
+                //that do not work, i dont like both solutions
+                //so save tried access point ssids in a small queue and cycle throught recently tried ssids//FIX THIS LATER
+
+
+
+
+               // wifiManager.addNetworkSuggestions(suggestionsList)
+
+                delay(8000L)//wait 8 seconds to allow wifi login application to work
+
+                var test1 = false
+                test1 = isDeviceOnline(this)
+
+                while (test1){
+                    test1 = isDeviceOnline(this)
+                    delay(1000L)
+
+                }
+
+
+
+                //check if there is network connection
+                //if no then scan again
+
+                //if yes then all is fine
+                    //check again in 5 seconds
+
+
+                }
+                    //if some passwordless hotspots are found then use them, if not use mobile data
+                    //in android when wifi and mobile data is available then it usually uses wifi
+                    //so having mobile data active all the time works fine
+
+
+                    //pick 3 paswordless wifis at random and add them to the list
+
+                    //wait for it to connect and for wifi web login app to kick in
+
+                    //check if there is a connection to the internet
+                    //if no then scan again
+                    //if yes then all is fine
+
+            //save data in a csv file here
+            //location ,time , battery %, try cpu temp, wifi name connected to, was the connection sucesfull?, list of other available wifis
+            //another file for test app
+            //another file to save how long the sucesfull conections lasted for
+
+                    //when connection fails scan again
+
+
+            Log.d("test1", "12345")
+            val folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val file = File(folder, "testor.csv")
+            val stream = FileOutputStream(file, true)
+            var newText = "\n oga, boga"
+            // Write the new text to the file
+            stream.write(newText.toByteArray())
+            // Close the stream
+            stream.close()
+            Log.d("test1", "123456")
+
+
+
+                    while (TimeNow < TimeFuture) {
+                        Log.d("test1", "oga")
+                        TimeNow = System.currentTimeMillis()
+                        delay(1000L)
+                    }
+
+                }
+
+        Log.d("test1", "oga")
+        TimeFuture = TimeNow + 1
+
+
+        delay(1000L)
+
+    }
 
 
 
@@ -128,6 +393,7 @@ class MainActivity : AppCompatActivity() {
         val checkPermission4 = checkSelfPermission(Manifest.permission.CHANGE_WIFI_STATE)
         val checkPermission5 = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
         val checkPermission6 = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
         val needToAsk =
             !((checkPermission1 == PackageManager.PERMISSION_GRANTED) and (checkPermission2 == PackageManager.PERMISSION_GRANTED) and (checkPermission3 == PackageManager.PERMISSION_GRANTED) and (checkPermission4 == PackageManager.PERMISSION_GRANTED)and (checkPermission5 == PackageManager.PERMISSION_GRANTED)and (checkPermission6 == PackageManager.PERMISSION_GRANTED))
         if (needToAsk) {
