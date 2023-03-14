c8ef1405ce4d4c7cbe6e2bd5e4120fe1c4c50e2e
Slepy <92794194+Slepy1@users.noreply.github.com>
2023-03-14 15:51:46 +0000

    added amplify functions
    todo, optimize code, add 1 more csv file, set up first experiment with amplify

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,31 +6,44 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.location.Location
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.net.wifi.WifiNetworkSuggestion
+import android.os.BatteryManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.api.aws.AWSApiPlugin
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.datastore.AWSDataStorePlugin
+import com.amplifyframework.datastore.generated.model.Todo
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.io.*
-import java.nio.file.Paths
-import java.time.Duration
-import java.time.LocalDate
-import java.time.temporal.ChronoUnit
+import java.io.File
+import java.io.FileOutputStream
+import java.time.LocalDateTime
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 var stopClicked = false
+private lateinit var fusedLocationClient: FusedLocationProviderClient
+var ogaboga = "abc"
+var recentlyTried: Queue<String> = LinkedList<String>()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +52,27 @@ class MainActivity : AppCompatActivity() {
 
         //check and/or ask for permissions
         checkPermissions()
+
+
+        //AWS Amplify datastore setup
+        //the datastore function is linked with dynamo db
+        //thanks to Faraday Solutions for allowing me to use their AWS credit
+        //remember to set min sdk to 32 otherwise gradle will die
+        try {
+            Amplify.addPlugin(AWSApiPlugin())
+            Amplify.addPlugin(AWSDataStorePlugin())
+            Amplify.configure(applicationContext)
+            Log.i("Amplify", "Initialized Amplify")
+        } catch (e: AmplifyException) {
+            Log.e("Amplify", "Could not initialize Amplify", e)
+        }
+
+
+
+
+
+
+
 
         //create some global values and managers
         //val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
@@ -55,10 +89,24 @@ class MainActivity : AppCompatActivity() {
         //scanWifi()
 
 
+        findViewById<Button>(R.id.amplifyButton).setOnClickListener {
+
+            amplifyCreate()
+            amplifyReadAll()
+        }
+
+
+
 
         findViewById<Button>(R.id.buttonStop).setOnClickListener {
 
             stopClicked = true
+
+        }
+
+        findViewById<Button>(R.id.resetButton).setOnClickListener {
+
+            recentlyTried.clear()
 
         }
 
@@ -142,8 +190,32 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    /////////////////////////////////amplify datastore functions
+    private fun amplifyCreate(){//this function saves to-do data model locally and syncs it with dynamodb when possible
+        val item: Todo = Todo.builder()//todo if possible add aws lambda  function to also record time at which the data was synced with aws
+            .name("testor")
+            .description("Lorem ipsum dolor sit amet")
+            .build()
+        Amplify.DataStore.save(
+            item,
+            { success -> Log.i("Amplify", "Saved item: " + success.item().name) },
+            { error -> Log.e("Amplify", "Could not save item to DataStore", error) }
+        )
+    }
 
-
+    private fun amplifyReadAll(){
+        Amplify.DataStore.query(
+            Todo::class.java,
+            { items ->
+                while (items.hasNext()) {
+                    val item = items.next()
+                    Log.i("Amplify", "Queried item: " + item.id + item.name)
+                }
+            },
+            { failure -> Log.e("Tutorial", "Could not query DataStore", failure) }
+        )
+    }
+    /////////////////////////////////no moreamplify datastore functions
 
 
     private fun isDeviceOnline(context: Context): Boolean {
@@ -204,7 +276,9 @@ class MainActivity : AppCompatActivity() {
         var ssids: Queue<String> = LinkedList<String>()
         var scanResults: MutableList<ScanResult>?
         var suggestionsList = ArrayList<WifiNetworkSuggestion>()
-        var recentlyTried: Queue<String> = LinkedList<String>()
+        //var recentlyTried: Queue<String> = LinkedList<String>()
+        ogaboga = "error"
+        wifiManager.removeNetworkSuggestions(suggestionsList)
 
         while (!stopClicked){
 
@@ -255,23 +329,52 @@ class MainActivity : AppCompatActivity() {
                     run breaking@ {
                     ssids?.forEach {//a fancy loop for queues
                             x ->
-                        recentlyTried.forEach { y ->
-                            if (x == y) {
-                                //do nothing
-                            }
+
+                        if (recentlyTried.size == 0){
                             recentlyTried.add(x)
-                            //add to the list
-                            if (recentlyTried.size > 3){
-                                recentlyTried.remove()
-                            }
-                            //if the list is more than 3 pop 1
-                            //try
+                            Log.d("test1", "connecting to " + x.toString())
                             wifiManager.addNetworkSuggestions(suggestionsList)
                             val suggestWifi = WifiNetworkSuggestion.Builder()
                                 .setSsid(x)
                                 .build()
+                            ogaboga = x.toString()
                             suggestionsList.add(suggestWifi)
+                            wifiManager.addNetworkSuggestions(suggestionsList)
                             return@breaking
+                        }
+                        else {
+
+
+                        recentlyTried.forEach { y ->
+                            //Log.d("test1", "y- " + y.toString())
+                            //Log.d("test1", "x- " + x.toString())
+                            if (x == y) {
+                                //do nothing
+                                Log.d("test1", x.toString() + "was recently tried, next")
+
+                            }
+                            else {
+                            //add to the list
+                            //if the list is more than 3 pop 1
+                            //try
+                                recentlyTried.add(x)
+                                if (recentlyTried.size > 3){
+                                    recentlyTried.remove()
+                                }
+
+
+                                Log.d("test1", "connecting to " + x.toString())
+                            val suggestWifi = WifiNetworkSuggestion.Builder()
+                                .setSsid(x)
+                                .build()
+                            ogaboga = x.toString()
+                            suggestionsList.add(suggestWifi)
+                                wifiManager.addNetworkSuggestions(suggestionsList)
+                            //ogaboga = x.toString()
+                            return@breaking
+                            }
+
+                        }
                         }
                     }
 
@@ -296,7 +399,7 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("test1", "123")
             if (suggestionsList.size > 0){
-                Log.d("test1", "1234")
+                Log.d("test1", "waiting")
 
 
                 //problem ahead
@@ -312,12 +415,12 @@ class MainActivity : AppCompatActivity() {
 
                // wifiManager.addNetworkSuggestions(suggestionsList)
 
-                delay(8000L)//wait 8 seconds to allow wifi login application to work
-
+                delay(18000L)//wait 8 seconds to allow wifi login application to work
+                Log.d("test1", "stop waiting")
                 var test1 = false
                 test1 = isDeviceOnline(this)
 
-                while (test1){
+                while (test1){//does not work ?? idk now
                     test1 = isDeviceOnline(this)
                     delay(1000L)
 
@@ -356,11 +459,76 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("test1", "12345")
             val folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-            val file = File(folder, "testor.csv")
+            val file = File(folder, "test1234.csv")
             val stream = FileOutputStream(file, true)
-            var newText = "\n oga, boga"
+            //var location1: Location
+            var location1 = Location("dummyprovider")
+            location1.setLatitude(20.3);
+            location1.setLongitude(52.6);
+
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+          //  fusedLocationClient.lastLocation
+              //  .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                   // Log.d("test1", "1234567")
+                   // Log.d("test1", location.toString())
+
+              //  }
+
+
+            fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+                override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+                override fun isCancellationRequested() = false
+            })
+                .addOnSuccessListener { location: Location? ->
+                    if (location == null)
+                        Toast.makeText(this, "Cannot get location.", Toast.LENGTH_SHORT).show()
+                    else {
+                       // val lat = location.latitude
+                       // val lon = location.longitude
+                        Log.d("test1", location.toString())
+                        location1 = location
+                        //Log.d("test1", location.toString())
+                    }
+
+                }
+
+            delay(4000L)
+
+
+
+
+
+            //get current battery level using battery manager and a sticky intent
+            val ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            val batteryStatus = registerReceiver(null, ifilter)
+            val level = batteryStatus!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = batteryStatus!!.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            val batteryPct = level / scale.toFloat()
+            val x = (batteryPct * 100).toInt()
+            //Toast.makeText(this, x.toString(), Toast.LENGTH_SHORT).show()
+
+
+                var tester45 = ""
+                //tester45 = tester45.replace(",", "testor")
+
+            scanResults?.forEach {
+                    d ->
+
+                var igabiga = d.toString().replace(",", ".")
+
+                     tester45 = tester45 + igabiga + ", ," }
+
+//if error its using mobile data, simple as
+
+            var current = LocalDateTime.now()
+            var newText = "\n " + current + "," + "\"" + location1.toString() + "\""  + ","  + x.toString() + "," + ogaboga + "," + tester45
             // Write the new text to the file
             stream.write(newText.toByteArray())
+
+
             // Close the stream
             stream.close()
             Log.d("test1", "123456")
